c9dbfd210f9639dcd7410b8b396be272cca6541a
munail <munailahmed003@gmail.com>
2017-02-23 12:40:30 +0300

    year

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
+var leapYear = function(year) {
+    return false;
+  };
+
+  // jQuery user interface logic here.
+var leapYear = function(year) {
+    if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
